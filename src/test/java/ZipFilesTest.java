6f80a0615c6ef56efa103812e1fdbfedb97cc9f8
Valentin1337 <deathman777@gmail.com>
2022-04-01 00:56:39 +0400

    last changes

--- src/test/java/ZipFilesTest.java
+++ src/test/java/ZipFilesTest.java
@@ -15,13 +15,17 @@ public class ZipFilesTest {
     private static final String CSV_NAME = "csvExample.csv";
     private static final String XLSX_NAME = "xlxsExample.xlsx";
     private static final String PDF_NAME = "pdfExample.pdf";
-    ZipFile zipFile = new ZipFile("src/test/resources/files/zipExample.zip");
+
+    ZipFile getZipFile() throws Exception {
+        ZipFile zipFile = new ZipFile("src/test/resources/files/zipExample.zip");
+        return zipFile;
+    }
 
 
     @Test
     void testPdfFile() throws Exception {
-        ZipEntry entry = zipFile.getEntry(PDF_NAME);
-        try (InputStream is = zipFile.getInputStream(entry)) {
+        ZipEntry entry = getZipFile().getEntry(PDF_NAME);
+        try (InputStream is = getZipFile().getInputStream(entry)) {
             PDF pdf = new PDF(is);
             assertThat(pdf.numberOfPages).isEqualTo(2);
             assertThat(pdf.text).contains("little more text");
@@ -30,8 +34,8 @@ public class ZipFilesTest {
 
     @Test
     void testXlxsFile() throws Exception {
-        ZipEntry entry = zipFile.getEntry(XLSX_NAME);
-        try (InputStream is = zipFile.getInputStream(entry)) {
+        ZipEntry entry = getZipFile().getEntry(XLSX_NAME);
+        try (InputStream is = getZipFile().getInputStream(entry)) {
             XLS xls = new XLS(is);
             assertThat(xls.excel
                     .getSheetAt(0)
@@ -44,8 +48,8 @@ public class ZipFilesTest {
 
     @Test
     void testCSVFile() throws Exception {
-        ZipEntry entry = zipFile.getEntry(CSV_NAME);
-        try (InputStream is = zipFile.getInputStream(entry)) {
+        ZipEntry entry = getZipFile().getEntry(CSV_NAME);
+        try (InputStream is = getZipFile().getInputStream(entry)) {
             CSVReader reader = new CSVReader(new InputStreamReader(is));
             List<String[]> csv = reader.readAll();
             assertThat(csv.get(1)).contains("booker12;9012;Rachel;Booker");
